441fd931abe0c8e15bb4342a00ebc4350205e0df
Andik <andik.aryantoo@gmail.com>
2019-12-30 02:24:06 -0700

    linux checkout

--- App/Config/Database.php
+++ App/Config/Database.php
@@ -5,7 +5,7 @@
  */
 $db['default'] = array(
     'connectionstring' => "",
-    'host' => 'localhost',
+    'host' => '127.0.0.1',
     'user' => 'root',
     'password' => '',
     'dbname' => 'pos',
